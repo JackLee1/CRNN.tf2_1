e04175530dd8fa77853751c960dbe7f7498e030d
Johnson0213 <johnson0213.am07@nctu.edu.tw>
2021-10-22 16:06:06 +0800

    convert to fp32 fp16 int8 tflite ver1

--- crnn/tflite_converter_int8.py
+++ crnn/tflite_converter_int8.py
@@ -11,14 +11,14 @@ print(tf.__version__)
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=Path, required=True, help='The config file path.')
 parser.add_argument('--pre', type=str, help='pre processing.')
-parser.add_argument('--weight',type=str,required=True,help='directory of weight')
+parser.add_argument('--weight',type=str, required=True, help='directory of weight')
+parser.add_argument('--export_dir',type=str, required=True, help='directory of export dir')
 parser.add_argument('--fp32', default=False, action="store_true")
 parser.add_argument('--fp16', default=False, action="store_true")
 parser.add_argument("--int8", default=False, action="store_true")
 
 args = parser.parse_args()
-args.output_dir='export_dir'
-os.makedirs(args.output_dir, exist_ok=True)
+os.makedirs(args.export_dir, exist_ok=True)
 
 dir_list=['pb_dir']
 if args.fp32: dir_list.append('tflite_fp32')
@@ -26,8 +26,7 @@ if args.fp16: dir_list.append('tflite_fp16')
 if args.int8: dir_list.append('tflite_int8')
 
 for subdir in dir_list:
-    os.makedirs(os.path.join(args.output_dir, subdir), exist_ok=True)
-
+    os.makedirs(os.path.join(args.export_dir, subdir), exist_ok=True)
 
 with args.config.open() as f:
     config = yaml.load(f, Loader=yaml.Loader)['dataset_builder']
@@ -36,27 +35,27 @@ with open(config['table_path']) as f:
     num_classes = len(f.readlines())
 
 # Testing Load and Inference pb graph
-loaded=tf.saved_model.load(os.path.join(args.weight, 'pb_dir'))
+loaded=tf.saved_model.load(args.weight)
 print(list(loaded.signatures.keys()))
 infer = loaded.signatures[list(loaded.signatures.keys())[0]]
-print(infer.structured_outputs)
+print('infer.structured_outputs', infer.structured_outputs)
 
 # pb Inference
 image = np.zeros((48,200,3))
-print(image.shape)
 image = (np.expand_dims(image, axis=0) / 255.0).astype(np.float32) # (1,14,63,3)
+print('input image detaial', image.shape, image.dtype)
 result = loaded(image)
 print("successfully do inference in savemodel")
-print(np.argmax(result, axis=-1))
+print('result of output', result.shape, result.dtype)
 print("CRNN has {} trainable variables, ...".format(len(loaded.trainable_variables)))
-for v in loaded.trainable_variables:
-    print(v.name)
+# for v in loaded.trainable_variables:
+#     print(v.name)
 
 if args.fp32:
     ##########################################
     # Load save_model and Convert to TFlite  #
     ##########################################
-    converter = tf.lite.TFLiteConverter.from_saved_model(os.path.join(args.weight))
+    converter = tf.lite.TFLiteConverter.from_saved_model(args.weight)
     tflite_model = converter.convert()
     print('successfully convert tflite fp32 model')
 
@@ -84,14 +83,16 @@ if args.fp32:
     print(output.keys())
     # 'output' is dictionary with all outputs from the inference.
     # In this case we have single output 'result'.
-    print(output['output_0'].shape)
+    print(output['output_0'].shape, output['output_0'].dtype)
     print('successfully inference in tflite fp32 model')
 
 if args.fp16:
     ##########################################
     # Load save_model and Convert to TFlite  #
     ##########################################
-    converter = tf.lite.TFLiteConverter.from_saved_model(os.path.join(args.weight))
+    converter = tf.lite.TFLiteConverter.from_saved_model(args.weight)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    converter.target_spec.supported_types = [tf.float16]
     tflite_model = converter.convert()
     print('successfully convert tflite fp16 model')
 
@@ -116,7 +117,7 @@ if args.fp16:
 
     my_signature = interpreter.get_signature_runner()
     # my_signature is callable with input as arguments.
-    output = my_signature(x=tf.constant([1.0], shape=(1,48,200,3), dtype=tf.float16))
+    output = my_signature(x=tf.constant([1.0], shape=(1,48,200,3), dtype=tf.float32))
     print(output.keys())
     # 'output' is dictionary with all outputs from the inference.
     # In this case we have single output 'result'.
@@ -127,12 +128,12 @@ if args.int8:
     ##########################################
     # Load save_model and Convert to TFlite  #
     ##########################################
-    train_images=np.zeros((200, 48, 200, 3))
+    train_images=np.zeros((200, 48, 200, 3), dtype=np.float32)
     def representative_data_gen():
       for input_value in tf.data.Dataset.from_tensor_slices(train_images).batch(1).take(100):
         yield [input_value]
       
-    converter = tf.lite.TFLiteConverter.from_saved_model(os.path.join(args.weight))
+    converter = tf.lite.TFLiteConverter.from_saved_model(args.weight)
     converter.optimizations = [tf.lite.Optimize.DEFAULT]
     converter.experimental_enable_resource_variables = True
     converter.representative_dataset = representative_data_gen
@@ -168,9 +169,11 @@ if args.int8:
     print('int8 output_details', output_details)
 
     interpreter.allocate_tensors()
-    interpreter.set_tensor(input_details["index"], np.zeros((1,48,200,3)))
+    interpreter.set_tensor(input_details["index"], np.zeros((1,48,200,3), dtype=np.int8))
     interpreter.invoke()
-    output = interpreter.get_tensor(output_details["index"])[0]
+    output = interpreter.get_tensor(output_details["index"])
 
-    print(output['output_0'].shape)
+    print(output.shape)
     print('successfully inference in tflite int8 model')
+
+
